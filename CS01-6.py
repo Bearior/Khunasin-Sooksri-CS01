b6726d195023a32c18dd582e43273528cd77ce17
Bearior <zazamheemook@gmail.com>
2021-07-16 09:29:16 +0700

    update

--- CS01-6.py
+++ CS01-6.py
@@ -1,3 +1,6 @@
-A = int(input("Any number here :"))
-if(A<10):
+A = int(input("A :"))
+B = int(input("B :"))
+C = int(input("C :"))
+
+if(A<10 | B>5 and C<6):
     print("Yes")
---
+++ CS01-7.py
@@ -0,0 +1,11 @@
+A = int(input("A :"))
+
+if A<=10:
+    print("fail")
+elif A<=20:
+    print("improve")
+elif A<=30:
+    print("Very good")
+else :
+    print("aroi")
+
