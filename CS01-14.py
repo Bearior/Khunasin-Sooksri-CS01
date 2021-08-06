aec595b63c2b4822df98165188dba33b7ffaa383
Bearior <zazamheemook@gmail.com>
2021-08-06 10:18:07 +0700

    e

--- CS01-14.py
+++ CS01-14.py
@@ -1,6 +1,6 @@
-thislist = [5,10,15,20,25,50,20]
-for i in range(len(thislist)):
+Aroi = [5,10,15,20,25,50,20]
+for i in range(len(Aroi)):
     print(i)
-    if (thislist[i] == 20):
-        thislist[i] = 200
-print (thislist)
+    if (Aroi[i] == 20):
+        Aroi[i] = 200
+print (Aroi)
