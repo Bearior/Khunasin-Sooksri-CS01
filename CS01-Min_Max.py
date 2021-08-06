902553d4b65aecc3e2563abfc7cdd084ad7a709d
Bearior <zazamheemook@gmail.com>
2021-08-06 08:28:50 +0700

    minmax

--- CS01-Min_Max.py
+++ CS01-Min_Max.py
@@ -3,7 +3,7 @@ Numtotal = []
 for i in range(Num):
     data = int(input('Enter data:'))
     Numtotal += [data]
-        Numtotal.sort(reverse=False)
-        print(Num[0])
-        nvm = len(Num)
-        print(Num[nvm-1])
+Numtotal.sort(reverse=False)
+print("min :",Numtotal[0])
+nvm = len(Numtotal)
+print("max :",Numtotal[nvm-1])
